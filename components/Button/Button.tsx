5b15f463d2196efa2642ab108d600fc44bb5f09c
Kirill <kirill.baikin@gmail.com>
2021-11-10 14:46:22 +0300

    Fixed buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,13 +1,15 @@
 import styles from './Button.module.scss'
+import React from "react";
 
-export default function (props){
+export default function (props) {
 
   // @ts-ignore
-  const blackButton=(
-    <div style={props.style} className={styles.parent} type={"blackButton"}>
+  const blackButton = (
+    <div style={props.style} className={styles.blackButton}>
       {props.children}
     </div>
   )
-
-  return(blackButton)
+  return (
+    blackButton
+  )
 }
