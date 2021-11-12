242c6b2dd1f6c83b66ff7c65f7f861ce7ed3572b
Kirill <kirill.baikin@gmail.com>
2021-11-12 18:31:09 +0300

    Added blog

--- components/Blog/Blog.tsx
+++ components/Blog/Blog.tsx
@@ -1,13 +1,67 @@
 import styles from './Blog.module.scss'
+import Input from "../Input/Input";
 
-export default function (props){
+export default function (props) {
+
+
+  return (
+    <div id={props.name} className={styles.parent}>
+      <h1>Новости</h1>
+      <div className={styles.blogParent}>
+        <div className={styles.content}>
+          <BlogElement/>
+          <BlogElement/>
+          <BlogElement/>
+          <BlogElement/>
+          <BlogElement/>
+          <BlogElement/>
+          <BlogElement/>
+        </div>
+        <div className={styles.paramsParent}>
+          <div className={styles.stickyContainter}>
+            <Input placeholder={"Поиск по статьям"}/>
+            <h1>Тэги</h1>
+            <div className={styles.tags}>
+
+            </div>
+            <hr/>
+            <h1>Ссылки</h1>
+            <div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+
+const BlogElement = () => {
+
+  return (
+    <div className={styles.blogElement}>
+      <div className={styles.img}/>
+      <div className={styles.tags}>
+        <Tag title={"бла бла бла"}/>
+        <Tag title={"бла бла бла"}/>
+        <Tag title={"бла бла бла"}/>
+        <Tag title={"бла бла бла"}/>
+      </div>
+      <h1>Заголовок</h1>
+      <div className={styles.date}>14.11.2002</div>
+      <div className={styles.desc}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur cupiditate ea fugiat, illo incidunt
+        labore magnam natus nisi non optio ratione rerum sed sequi. Fugiat illo neque non sequi tenetur?
+      </div>
+    </div>
+  )
+}
+
+const Tag=(props)=>{
 
   return(
-    <div id={props.name} className={styles.parent}>
-      <h1>Блог</h1>
-      <div>
-        Блога пока нет, но он скоро обязательно будет
-      </div>
+    <div className={styles.tag}>
+      {props.title}
     </div>
   )
 }
